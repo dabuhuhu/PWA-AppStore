cf108adb1afe3c1ca9d21525bd6dad70263b2ee5
Timotheus Wiese <80679542+dabuhuhu@users.noreply.github.com>
2023-10-05 12:01:59 +0200

    hehe

--- script.js
+++ script.js
@@ -1,21 +1,10 @@
-function checkPWA() {
+function checkPWAForiOS() {
     const domain = document.getElementById('domainInput').value;
 
-    // Check for service worker support
-    if ('serviceWorker' in navigator) {
-        // Check for manifest file
-        fetch(`https://${domain}/manifest.json`)
-            .then(response => {
-                if (response.status === 200) {
-                    document.getElementById('result').textContent = `${domain} can be installed as a PWA.`;
-                } else {
-                    document.getElementById('result').textContent = `${domain} does not have a valid manifest file.`;
-                }
-            })
-            .catch(error => {
-                document.getElementById('result').textContent = `${domain} could not be reached or does not have a valid manifest file.`;
-            });
+    // Check for iOS-specific PWA features
+    if ('standalone' in window.navigator && window.navigator.standalone) {
+        document.getElementById('result').textContent = `${domain} can be installed as a PWA on iOS.`;
     } else {
-        document.getElementById('result').textContent = `${domain} cannot be installed as a PWA because the browser does not support service workers.`;
+        document.getElementById('result').textContent = `${domain} cannot be installed as a PWA on iOS.`;
     }
 }
